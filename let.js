bb474218db15be8315f17a3709b3f221c8639661
Dayanç <33333043+blacknbunny@users.noreply.github.com>
2017-11-06 03:30:39 +0300

    Update let.js

--- let.js
+++ let.js
@@ -6,6 +6,9 @@ let degisken = “Bu bir degisken”;
 
 /* Örnek : */
 
-for(let i = 0;i < 5;i++){ /* let i değişkeni bloğunda çalıştı ve bitti artık burada yeni atama yapabiliriz i değişkeni üzerine. */ let i = “yeni”; }
+for(let i = 0;i < 5;i++){
+  /* let i değişkeni bloğunda çalıştı ve bitti artık burada yeni atama yapabiliriz i değişkeni üzerine. */
+  let i = “yeni”;
+}
 
 /* Yukarıdaki örnekte let i değişkeni ilk döngü bloğunda kullanılmış ve yeni bir atama yapılabilir hale gelmiştir. */
