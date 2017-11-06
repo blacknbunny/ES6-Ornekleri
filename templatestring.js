ac2c3a6401c70293bc5cb6b37e91df3867579c9e
Dayanç <33333043+blacknbunny@users.noreply.github.com>
2017-11-06 03:26:13 +0300

    Create templatestring.js

---
+++ templatestring.js
@@ -0,0 +1,11 @@
+Template String’leri
+
+/* ES6’da gelen ve bizi çok rahatlatan bir diğer özellik ise `` tırnakları */
+
+/* Eskiden ekrana yazılar ile degisken yazdırmak isteseydik */ function dayanc(isim=”Dayanç”, sifre=”dayanc123”){ console.log(“Benim ismim “ + isim + “ şifrem ise “ + sifre), }
+
+/* Şimdi yazdırmak istesek */
+
+function dayanc(isim=”Dayanç”, sifre=”dayanc123”){ console.log(Benim ismim ${isim} şifrem ise ${sifre}); }
+
+/* Artık `` tırnakları ile yazılara boşluk bırakma + işareti koyup ekleme zahmeti bitti. */
