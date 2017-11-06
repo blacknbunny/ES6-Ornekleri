edfca134caa512a1dce8c9e0ff1575103d94ffc5
Dayanç <33333043+blacknbunny@users.noreply.github.com>
2017-11-06 03:24:44 +0300

    Create const.js

---
+++ const.js
@@ -0,0 +1,11 @@
+Const değişkeni
+
+/* Const değişken türü bulunduğu kod bloğu içerisinde asla değiştirlemez. */
+
+const degisken = “Bu bir degisken”;
+
+/* Farklı bir kod bloğu arasına alırsak aynı değişken ismini iki farklı şekilde kullanabiliriz */
+
+/* Örnek : */ function bubirfonksiyon(){ const degisken = “Bu bir ikinci degisken”; }
+
+/* Yukarıdaki değişkenle aynı isimi aldılar fakat farklı kod bloğunda oldukları için aynı sayılmayacaklar. */
