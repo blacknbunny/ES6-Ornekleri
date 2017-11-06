6467d6a8b59f474e6bdee29278bb7729e0201a2e
Dayanç <33333043+blacknbunny@users.noreply.github.com>
2017-11-06 03:25:26 +0300

    Create fonksiyonparametre.js

---
+++ fonksiyonparametre.js
@@ -0,0 +1,9 @@
+Fonksiyon parametreleri
+
+/* ES6 ile gelen bir sürü özellik arasında en çok ilgimi çeken bu oldu. */
+
+function dayanc(isim=”dayanç”, sifre=”dayanc123”){
+
+}
+
+/* İsim ve Şifre parametresine yapılan atamalar eğer öyle bir parametre girilmez ise belirlediğimizi ata demek oluyor. */
