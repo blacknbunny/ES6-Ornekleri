67762da0996790ae747dbfe63dee039c2551fbc7
Dayanç <33333043+blacknbunny@users.noreply.github.com>
2017-11-06 03:25:03 +0300

    Update let.js

--- let.js
+++ let.js
@@ -1,1 +1,11 @@
+Let değişkeni
 
+/* Let değişken türü sadece bulunduğu kod bloğu içerisinde değiştirilebilir. */
+
+let degisken = “Bu bir degisken”;
+
+/* Örnek : */
+
+for(let i = 0;i < 5;i++){ /* let i değişkeni bloğunda çalıştı ve bitti artık burada yeni atama yapabiliriz i değişkeni üzerine. */ let i = “yeni”; }
+
+/* Yukarıdaki örnekte let i değişkeni ilk döngü bloğunda kullanılmış ve yeni bir atama yapılabilir hale gelmiştir. */
