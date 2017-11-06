62ede1ba488d285bb7298582963d7fd1db61c581
Dayanç <33333043+blacknbunny@users.noreply.github.com>
2017-11-06 03:25:41 +0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+Array operatorü
+
+/* Artık arraylarda tek tek çağırmak yerine hepsini birden çağırma özelliği var. */
+
+var array = [‘Ham1’, ‘Ham2’, ‘Ham3’];
+
+/* Aşşağıdaki örnek ile array içerisindeki tüm verileri alabiliyoruz. */
+
+console.log(…array);
