03108f95110765806e34fd30778fe6b242998de7
Dayanç <33333043+blacknbunny@users.noreply.github.com>
2017-11-06 03:32:19 +0300

    Update objeler.js

--- objeler.js
+++ objeler.js
@@ -1,19 +1,40 @@
-Yeni obje eklemeleri /* ES6 ile gelen objeler ve obje içerisindeki fonksiyonlar çok değişti. */
+Yeni obje eklemeleri
 
-var degisken1 = “Bu ilk degisken”; var degisken2 = “Bu ikinci degisken”;
+/* ES6 ile gelen objeler ve obje içerisindeki fonksiyonlar çok değişti. */
 
-/* Eski hali : */ var obje = { sec1 : degisken1, sec2 : degisken2, dayanc: function(param){ console.log(param); } };
+var degisken1 = “Bu ilk degisken”;
+
+var degisken2 = “Bu ikinci degisken”;
+
+/* Eski hali : */
+var obje = {
+  sec1 : degisken1,
+  sec2 : degisken2,
+  dayanc: function(param){
+    console.log(param);
+  }
+};
 
 console.log(obje.dayanc(“Merhaba”));
 
 /* Yeni hali : */
 
-var obje = { degisken1, degisken2,
+var obje = {
+  degisken1,
+  degisken2,
+  dayanc(param){
+    console.log(param);
+  }
+}
 
-dayanc(param){ console.log(param); } } console.log(obje.dayanc(“Merhaba”));
+console.log(obje.dayanc(“Merhaba”));
 
 /* Aralarındaki farklar : */
 
-//Eski halinde obje içerisinde : kullanarak değişken belirtmemiz gerekiyordu. //Obje içerisinde fonksiyonlara isim: function() diye başlamamız gerekiyordu.
+//Eski halinde obje içerisinde : kullanarak değişken belirtmemiz gerekiyordu.
 
-// Yeni halinde sadece degiskenleri yazıp virgül ile ayırdık. // Fonksiyonları kısaltılmış şekilde yazdık.
+//Obje içerisinde fonksiyonlara isim: function() diye başlamamız gerekiyordu.
+
+// Yeni halinde sadece degiskenleri yazıp virgül ile ayırdık.
+
+// Fonksiyonları kısaltılmış şekilde yazdık.
